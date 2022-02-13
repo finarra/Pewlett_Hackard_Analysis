27137fba0e236a479f0d3fd62c1a5b05c2a94bb6
finarra <95982833+finarra@users.noreply.github.com>
2022-02-13 14:55:41 -0600

    Update Employee_database_challenge.sql

--- Queries/Employee_database_challenge.sql
+++ Queries/Employee_database_challenge.sql
@@ -20,3 +20,20 @@ into retiring_titles
 from unique_titles
 group by title
 order by count(title) desc
+
+--Deliverable 2: mentorship eligibility
+select distinct on (e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+into mentorship_eligibility
+from employees as e
+left join dept_emp as de
+on e.emp_no = de.emp_no
+right join titles as t
+on t.emp_no=e.emp_no
+where de.to_date = '9999-01-01'
+and (e.birth_date between '1965-01-01' and '1965-12-31')
